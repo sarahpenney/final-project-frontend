6c459c5376808baaedc70d389945709382a24053
Sarah Penney <sarahpenneyuk@gmail.com>
2016-12-09 16:21:45 +0000

    Initial commit

---
+++ src/js/controllers/auth.js
@@ -0,0 +1,46 @@
+angular.module('finalProject')
+  .controller('RegisterController', RegisterController)
+  .controller('LoginController', LoginController);
+
+RegisterController.$inject = ['$auth', '$state'];
+function RegisterController($auth, $state) {
+
+  const register = this;
+
+  register.user = {};
+
+  function submit() {
+    console.log('Yo',register.user);
+    $auth.signup(register.user)
+      .then(() => {
+        console.log('pew pew');
+        $state.go('login');
+      });
+  }
+
+  register.submit = submit;
+}
+
+LoginController.$inject = ['$auth', '$state'];
+function LoginController($auth, $state) {
+  const login = this;
+
+  login.credentials = {};
+
+  function submit() {
+    $auth.login(login.credentials)
+      .then(() => {
+        $state.go('usersIndex');
+      });
+  }
+
+  function authenticate(provider) {
+    $auth.authenticate(provider)
+    .then((res) => {
+      console.log(res);
+    });
+  }
+
+  login.submit = submit;
+  login.authenticate = authenticate;
+}
---
+++ src/js/controllers/main.js
@@ -0,0 +1,35 @@
+angular.module('finalProject')
+.controller('MainController', MainController);
+
+MainController.$inject = ['$auth', '$state', '$rootScope'];
+function MainController($auth, $state, $rootScope) {
+  const main = this;
+
+  main.isLoggedIn = $auth.isAuthenticated;
+  main.message = null;
+
+  function logout() {
+    $auth.logout()
+    .then(() => {
+      $state.go('usersIndex');
+    });
+  }
+
+  const protectedStates = ['usersEdit', 'usersNew'];
+
+  function secureState(e, toState) {
+    main.message = null;
+    console.log(toState);
+    if(!$auth.isAuthenticated() && protectedStates.includes(toState.name)) {
+      e.preventDefault();
+      $state.go('login');
+      main.message = 'You must be logged in to go there!';
+
+    }
+
+  }
+
+  $rootScope.$on('$stateChangeStart', secureState);
+
+  main.logout = logout;
+}
